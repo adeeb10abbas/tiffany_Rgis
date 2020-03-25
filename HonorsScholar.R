47b5941ae2746d46149fe6a772dda5bef446364c
Tiffany Buturla <tiffanybuturla@tiffanys-mbp-2.lan>
2020-03-24 20:13:47 -0400

    updates

--- HonorsScholar.R
+++ HonorsScholar.R
@@ -6,8 +6,8 @@ library(sp)
 library(GISTools)
 
 # get gis data about census tracts
-phil1 <- tracts(42, county = 101)
-phil <- fortify(phil1)
+philly_sp <- tracts(42, county = 101) # units for aland and awater are m^2
+philly_df <- fortify(phil1)
 
 # load in public GSI project data
 public_gsi <- st_read("Desktop/HonorsScholar/DataSets/GSI_Public_Projects_Point/GSI_Public_Projects_Point.shp")
@@ -15,19 +15,23 @@ public_gsi <- fortify(public_gsi)
 
 # plot public GIS projects and census tracts
 ggplot() + 
-  geom_polygon(aes(x = long, y = lat, group = group), data = phil) +
+  geom_polygon(aes(x = long, y = lat, group = group), data = philly_df) +
   geom_sf(aes(), data = public_gsi)
 
-# convert public_gsi to spatial points dataframe to be able to use open()
+# convert public_gsi to spatial points dataframe to be able to use poly.counts()
 public_sp <- as_Spatial(public_gsi)
-public_sp <- spTransform(public_sp, crs(phil1))
+public_sp <- spTransform(public_sp, crs(philly_sp))
 
 # count number of public project points per census tract
-public_count <- over(public_sp, phil1)
-count <- poly.counts(public_sp, phil1)
-count <- setNames(count, phil1@data$NAMELSAD)
+count <- poly.counts(public_sp, philly_sp)
+count <- setNames(count, philly_sp@data$NAMELSAD)
 count <-as.data.frame(count)
 colnames(count)[1] <- "points"
-colnames(count)[0] <- "NAMELSAD"
-public_count <- merge(x = public_count, y = count, "NAMELSAD", all.x = TRUE)
-View(public_count)
+count <- tibble::rownames_to_column(count, "NAMELSAD")
+
+# join together counts and shapes of census tracts as dataframe and calculate density of points for public
+public_count <- left_join(as.data.frame(philly_sp), count, by = "NAMELSAD")
+public_count$ALAND <- as.numeric(public_count$ALAND)
+public_count <- transform(public_count, density = points / ALAND)
+
+
